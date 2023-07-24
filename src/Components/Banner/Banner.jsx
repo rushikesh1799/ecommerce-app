1dd12ab540f1038d84b02e5849285c9f3c6a2f0b
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-07-24 19:18:45 +0530

    Update: added the banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Banner.css";
+import { useNavigate } from "react-router-dom";
 
 const Banner = () => {
+    const navigate = useNavigate();
+
     return (
-        <section className="banner-container">
+        <section
+            className="banner-container"
+            onClick={() => navigate("/products")}
+        >
             <img
                 className="banner_hero_img"
-                src="https://res.cloudinary.com/dwegb6a4s/image/upload/v1689663007/Banner-2_g9pckc.png"
+                src="https://res.cloudinary.com/dwegb6a4s/image/upload/v1690200985/Banner4_nyjtep.jpg"
                 alt=""
             />
         </section>

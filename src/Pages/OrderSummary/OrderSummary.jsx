b4acb4aa496ee293a427762b74f0f340b404e556
Rushikesh Bunge <68096217+rushikesh1799@users.noreply.github.com>
2023-07-25 13:33:08 +0530

    Update: added the animation on placing the order

--- src/Pages/OrderSummary/OrderSummary.jsx
+++ src/Pages/OrderSummary/OrderSummary.jsx
@@ -2,47 +2,55 @@ import React, { useContext } from "react";
 import Header from "../../Components/Header/Header";
 import { DataContext } from "../../context/DataProvider";
 import Product from "../../Components/Product/Product";
+import Lottie from "lottie-react";
 
 import "./OrderSummary.css";
+import orderSummary from "../../lottie-files/orderSummary.json";
 import { NavLink } from "react-router-dom";
 
 const OrderSummary = () => {
-    const { orderObj } = useContext(DataContext);
+    const { cartlist, orderObj } = useContext(DataContext);
     const { addressToDeliver, boughtItems, totalOrderValue } = orderObj;
     return (
         <div>
             <Header />
-            <h2>Order Summary</h2>
-            <p>Thank you for placing your order</p>
-            <div className="orderSummary_container">
-                <div className="productlist-main-cards-container">
-                    {boughtItems.map((product) => (
-                        <Product product={product} />
-                    ))}
+            <div>
+                {" "}
+                <div className="order-animation">
+                    <Lottie loop={false} animationData={orderSummary} />
                 </div>
-                <div className="orderSummary_text">
-                    <div>
-                        <b>Delivery Address: </b>
-                        <span>
-                            {addressToDeliver.address_line_1 +
-                                " " +
-                                addressToDeliver.address_line_2 +
-                                ", " +
-                                addressToDeliver.city +
-                                " - " +
-                                addressToDeliver.pincode +
-                                ", " +
-                                addressToDeliver.state +
-                                "."}
-                        </span>
+                <h2>Order Summary</h2>
+                <p>Thank you for placing your order</p>
+                <div className="orderSummary_container">
+                    <div className="productlist-main-cards-container">
+                        {boughtItems.map((product) => (
+                            <Product product={product} />
+                        ))}
                     </div>
-                    <div>
-                        <b>Total Amount Paid: </b>
-                        <span>{totalOrderValue}</span>
+                    <div className="orderSummary_text">
+                        <div>
+                            <b>Delivery Address: </b>
+                            <span>
+                                {addressToDeliver.address_line_1 +
+                                    " " +
+                                    addressToDeliver.address_line_2 +
+                                    ", " +
+                                    addressToDeliver.city +
+                                    " - " +
+                                    addressToDeliver.pincode +
+                                    ", " +
+                                    addressToDeliver.state +
+                                    "."}
+                            </span>
+                        </div>
+                        <div>
+                            <b>Total Amount Paid: </b>
+                            <span>{totalOrderValue}</span>
+                        </div>
                     </div>
                 </div>
+                <NavLink to="/products">Shop More!</NavLink>
             </div>
-            <NavLink to="/products">Shop More!</NavLink>
         </div>
     );
 };
